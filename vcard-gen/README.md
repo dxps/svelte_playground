1f66d8f488178d5cf757a7a8927d7ce7ba83d44f
devisions <marius.ileana@gmail.com>
2020-06-27 17:35:19 +0300

    vcard-gen > events & two-way binding

---
+++ vcard-gen/README.md
@@ -0,0 +1,19 @@
+# vCard Generator
+
+
+
+## Get started
+
+Install the dependencies:
+
+```bash
+cd svelte-app
+npm install
+```
+
+Start:
+
+- using included `run-dev.sh` script
+- or use the standard `npm run dev`
+
+Navigate to [localhost:5000](http://localhost:5000) to access the app.
---
+++ vcard-gen/src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import App from "./App.svelte";
+  import Navbar from "./Sections/Navbar.svelte";
+
+  let disabled = true;
+  let name = "";
+  let firstInput;
+
+  function keyPressed(event) {
+    console.log(event);
+    event.key === "Shift" ? "" : (disabled = false);
+  }
+  function resetClicked() {
+    disabled = true;
+    name = "";
+    firstInput.focus();
+  }
+</script>
+
+<Navbar title="vCard Generator" />
+<br />
+
+<form>
+  <label>
+    Name:
+    <input
+      type="text"
+      placeholder="Please enter your name"
+      on:keydown={keyPressed}
+      bind:value={name}
+      bind:this={firstInput} />
+  </label>
+  <button {disabled} on:click|preventDefault={resetClicked}>Reset</button>
+</form>
+
+<section>Name:{name}</section>
