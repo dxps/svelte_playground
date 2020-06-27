1e416d3626217f9ac2a47c7d64521cd2542e8e4f
devisions <marius.ileana@gmail.com>
2020-06-27 18:17:23 +0300

    vcard-gen > Form comp & conditional (css) classes

--- vcard-gen/src/App.svelte
+++ vcard-gen/src/App.svelte
@@ -1,36 +1,10 @@
 <script>
   import App from "./App.svelte";
   import Navbar from "./Sections/Navbar.svelte";
-
-  let disabled = true;
-  let name = "";
-  let firstInput;
-
-  function keyPressed(event) {
-    console.log(event);
-    event.key === "Shift" ? "" : (disabled = false);
-  }
-  function resetClicked() {
-    disabled = true;
-    name = "";
-    firstInput.focus();
-  }
+  import Form from "./UI/Form.svelte";
 </script>
 
 <Navbar title="vCard Generator" />
 <br />
 
-<form>
-  <label>
-    Name:
-    <input
-      type="text"
-      placeholder="Please enter your name"
-      on:keydown={keyPressed}
-      bind:value={name}
-      bind:this={firstInput} />
-  </label>
-  <button {disabled} on:click|preventDefault={resetClicked}>Reset</button>
-</form>
-
-<section>Name:{name}</section>
+<Form />
---
+++ vcard-gen/src/UI/Form.svelte
@@ -0,0 +1,42 @@
+<script>
+  let disabled = true;
+  let name = "";
+  let firstInput;
+  let active = true;
+
+  function keyPressed(event) {
+    console.log(event);
+    event.key === "Shift" ? "" : (disabled = false);
+  }
+  function resetClicked() {
+    disabled = true;
+    name = "";
+    firstInput.focus();
+  }
+</script>
+
+<style>
+  .active {
+    border-color: hsl(211, 63%, 35%);
+  }
+</style>
+
+<form>
+  <label>
+    Name:
+    <input
+      type="text"
+      placeholder="Please enter your name"
+      class:active
+      on:focus={() => {
+        active = true;
+      }}
+      on:blur={() => {
+        active = false;
+      }}
+      on:keydown={keyPressed}
+      bind:value={name}
+      bind:this={firstInput} />
+  </label>
+  <button {disabled} on:click|preventDefault={resetClicked}>Reset</button>
+</form>
