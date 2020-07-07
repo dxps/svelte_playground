15344504ba42d86f3a7420c853e10f4f765fe720
devisions <marius.ileana@gmail.com>
2020-07-07 12:43:06 +0300

    svelte-dialog > 1st commit

---
+++ svelte-dialog/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Dialog from "./Dialog.svelte";
+
+  let dialog;
+</script>
+
+<!-- UI -->
+
+<div>
+  <button on:click={() => dialog.showModal()}>Open Dialog</button>
+</div>
+
+<Dialog title="Title of the dialog" bind:dialog>
+  My content is very, veeeeeerryyyyy looooooooong.
+  <br />
+  And it is not wrapped by default.
+</Dialog>
