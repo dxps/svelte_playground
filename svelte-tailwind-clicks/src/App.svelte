267946c080520773c6aec9479d7d3ada00c6bf11
dxps <marius.ileana@gmail.com>
2022-01-16 16:56:37 +0200

    feat: svelte-tailwind-clicks added

---
+++ svelte-tailwind-clicks/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let count = 1
+
+  function increment() {
+    count++
+  }
+</script>
+
+<main class="flex flex-col justify-center items-center h-screen space-y-10">
+  {count}
+  <button on:click={increment} class="btn btn-primary">+</button>
+</main>
+
+<style>
+</style>
