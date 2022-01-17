2803daaf1e73eb5441f95853d2d83808e99cbcb9
dxps <marius.ileana@gmail.com>
2022-01-17 16:59:14 +0200

    feat(svelte-tailwind-clicks): reactive assignment

--- svelte-tailwind-clicks/src/App.svelte
+++ svelte-tailwind-clicks/src/App.svelte
@@ -1,14 +1,18 @@
 <script>
-  let count = 1
+  let number = 1
 
-  function increment() {
-    count++
+  function add5() {
+    number += 5
   }
+
+  // A reactive statement / assignment
+  $: multiple = number * 2
 </script>
 
 <main class="flex flex-col justify-center items-center h-screen space-y-10">
-  {count}
-  <button on:click={increment} class="btn btn-primary">+</button>
+  <p>The number is {number}.</p>
+  <button on:click={add5} class="btn btn-primary">Add Five</button>
+  <p>The number multiplied by 2 is {multiple}.</p>
 </main>
 
 <style>
