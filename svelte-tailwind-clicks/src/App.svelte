0c06659c3ccee10e61795d25a709184fccef0bef
dxps <marius.ileana@gmail.com>
2022-01-19 10:28:28 +0200

    feat(svelte-tailwind-clicks): form binding

--- svelte-tailwind-clicks/src/App.svelte
+++ svelte-tailwind-clicks/src/App.svelte
@@ -3,17 +3,47 @@
   import data from './data.json'
 
   let posts = data.data
+
+  let author = ''
+  let post = ''
+
+  function addPost() {
+    const _post = {
+      id: posts.length + 1,
+      author: author,
+      post: post,
+    }
+    posts.push(_post)
+    posts = posts
+    author = ''
+    post = ''
+  }
 </script>
 
 <main class="flex flex-col justify-center items-center h-screen space-y-10">
-  {#each posts as post}
-    <div class="card card-bordered w-80 bg-primary">
-      <div class="card-body">
-        <h2 class="card-title">{post.author}</h2>
-        <p>{post.post}</p>
+  <h1>Add Post</h1>
+  <div class="form-control">
+    <label for="author" class="label">
+      <span class="label-text">Author</span>
+    </label>
+    <input name="author" bind:value={author} type="text" placeholder="username" class="input input-bordered" />
+    <label for="post" class="label">
+      <span class="label-text">Post Text</span>
+    </label>
+    <textarea name="post" bind:value={post} class="textarea h-24 textarea-bordered" placeholder="" />
+    <button on:click={addPost} class="btn btn-primary mt-4">Post</button>
+  </div>
+
+  <div class="overflow-auto space-y-4 p-4">
+    {#each posts.reverse() as post}
+      <div class="card card-bordered bg-primary">
+        <div class="card-body">
+          <h2 class="card-title">{post.author}</h2>
+          <p>{post.post}</p>
+        </div>
       </div>
-    </div>
-  {/each}
+    {/each}
+  </div>
 </main>
 
 <style>
