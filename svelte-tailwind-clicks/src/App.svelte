23ffc32c6fd07b8548203372da7544c076d3b2b8
dxps <marius.ileana@gmail.com>
2022-01-19 09:21:42 +0200

    feat(svelte-tailwind-clicks): conditional rendering

--- svelte-tailwind-clicks/src/App.svelte
+++ svelte-tailwind-clicks/src/App.svelte
@@ -13,6 +13,36 @@
   <p>The number is {number}.</p>
   <button on:click={add5} class="btn btn-primary">Add Five</button>
   <p>The number multiplied by 2 is {multiple}.</p>
+
+  {#if multiple < 15}
+    <div class="alert alert-success">
+      <div class="flex-1">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+          />
+        </svg>
+        <label>Numbers look good!</label>
+      </div>
+    </div>
+  {:else}
+    <div class="alert alert-error">
+      <div class="flex-1">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+          />
+        </svg>
+        <label>Numbers are too high!</label>
+      </div>
+    </div>
+  {/if}
 </main>
 
 <style>
